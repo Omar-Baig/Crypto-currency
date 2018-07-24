da8d1ce4ac99277e49d080d88cf3b519718ab7f5
Omar-Baig <obaig992@gmail.com>
2018-07-24 11:22:54 -0400

    Fixed

--- crypto.js
+++ crypto.js
@@ -1,20 +1,40 @@
-var request = new XMLHttpRequest();
+var COIN_MARKET_CAP = new XMLHttpRequest();
 
-request.open("GET", "https://api.coinmarketcap.com/v2/ticker/?start=1&limit=20&sort=rank&structure=array", true);
-request.onload = function () {
+var COIN_MARKET_API = "https://api.coinmarketcap.com/v2/ticker/?start=1&limit=20&sort=rank&structure=array";
+
+COIN_MARKET_CAP.open("GET", COIN_MARKET_API, true);
+
+
+COIN_MARKET_CAP.onload = function () {
     //Access JSON data here
     var data = JSON.parse(this.response);
 
     function cCurrencyTemplate(coin) {
-        return `<tr>
-                    <td class="text-center">${coin.rank}</td>
-                    <td class="text-center">${coin.name}</td>
-                    <td class="text-center">${coin.symbol}</td>
-                    <td class="text-center">$ ${coin.quotes.USD.price}</td>
-                <tr>
-            `
+        var Name = coin.name;
+        var Rank = coin.rank;
+        var Price = coin.quotes.USD.price;
+        var OneHourChange = coin.quotes.USD.percent_change_1h;
+        var TwentyFourHourChange = coin.quotes.USD.percent_change_24h;
+        var SevenDayChange = coin.quotes.USD.percent_change_7d;
+
+        return `<div class="col-sm-6">
+                    <div class="card">
+                        <div class="coin-name text-center"><strong>${Name}</strong></div>
+                        <div class="coin-info text-center">
+                            <p><strong>Rank: </strong>${Rank}</p>
+                            <p><strong>Price: </strong>${Price}</p>
+                            <p><strong>% change 1h: </strong>${OneHourChange}</p>
+                            <p><strong>% change 24h: </strong>${TwentyFourHourChange}</p>
+                            <p><strong>% change 7d: </strong>${SevenDayChange}</p>
+                        </div>
+                    </div>
+                </div>`
     }
+
+    /* Bind to ID coins*/
     document.getElementById("coins").innerHTML =
         `${data.data.map(cCurrencyTemplate).join("")}`
 }
-request.send();
+
+
+COIN_MARKET_CAP.send();
